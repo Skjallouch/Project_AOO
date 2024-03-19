5b64e338af268b2727f8c7da783edd9beb87720f
teodoraistrate <teodoraistrate63@yahoo.com>
2024-03-19 10:58:55 +0100

    classe HabitudeAchat

---
+++ src/main/java/projetaoo/HabitudeAchat.java
@@ -0,0 +1,10 @@
+package projetaoo;
+
+import java.time.Month;
+import java.util.HashMap;
+
+public class HabitudeAchat {
+    HashMap<Month, Float> quantiteVetementsSM;
+    HashMap<Month, Float> quantiteVetementsPM;
+    HashMap<Month, Float> quantiteProduitsLocaux; // juste pour ne pas oublier l'idée
+}
