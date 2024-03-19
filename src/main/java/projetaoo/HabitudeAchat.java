bed18f4cbbc5481936add94709c0f8bd47b1f55d
teodoraistrate <teodoraistrate63@yahoo.com>
2024-03-19 11:11:27 +0100

    add/get methodes habitudes achat

--- src/main/java/projetaoo/HabitudeAchat.java
+++ src/main/java/projetaoo/HabitudeAchat.java
@@ -4,7 +4,44 @@ import java.time.Month;
 import java.util.HashMap;
 
 public class HabitudeAchat {
-    HashMap<Month, Float> quantiteVetementsSM;
-    HashMap<Month, Float> quantiteVetementsPM;
+    HashMap<Month, Integer> quantiteVetementsSM;
+    HashMap<Month, Integer> quantiteVetementsPM;
     HashMap<Month, Float> quantiteProduitsLocaux; // juste pour ne pas oublier l'idée
+   
+    public HashMap<Month, Integer> setQuantiteVetementsSM() {
+        return quantiteVetementsSM;
+    }
+
+    public HashMap<Month, Integer> getQuantiteVetementsPM() {
+        return quantiteVetementsPM;
+    }
+    
+    public HashMap<Month, Float> getQuantiteProduitsLocaux() {
+        return quantiteProduitsLocaux;
+    }
+
+    public void addProduitsLocaux(Month mois, Float kilosProduitsAchetes) {
+        if (this.quantiteProduitsLocaux.containsKey(mois)) {
+            this.quantiteProduitsLocaux.put(mois, this.quantiteProduitsLocaux.get(mois) + kilosProduitsAchetes);
+        } else {
+            this.quantiteProduitsLocaux.put(mois, kilosProduitsAchetes);
+        }
+    }
+
+    public void addVetementsSM(Month mois, Integer quantite) {
+        if (this.quantiteVetementsSM.containsKey(mois)) {
+            this.quantiteVetementsSM.put(mois, this.quantiteVetementsSM.get(mois) + quantite);
+        } else {
+            this.quantiteVetementsSM.put(mois, quantite);
+        }
+    }
+
+    public void addVetementsPM(Month mois, Integer quantite) {
+        if (this.quantiteVetementsPM.containsKey(mois)) {
+            this.quantiteVetementsPM.put(mois, this.quantiteVetementsPM.get(mois) + quantite);
+        } else {
+            this.quantiteVetementsPM.put(mois, quantite);
+        }
+    }
+    
 }
